ec171e7e8ace1abfd36c0b0310b93e3ccd11452d
marco76tv <marco.sottana@gmail.com>
2025-05-18 22:32:12 +0200

    .

--- lang/lang/it/failed_job.php
+++ lang/lang/it/failed_job.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+
 return [
     'actions' => [
         'retry_all' => [
--- resources/lang/it/job_batch.php
+++ resources/lang/it/job_batch.php
@@ -1,6 +1,4 @@
-<?php
-declare(strict_types=1);
-return array (
+<?php return array (
   'actions' => 
   array (
     'prune_batches' => 
